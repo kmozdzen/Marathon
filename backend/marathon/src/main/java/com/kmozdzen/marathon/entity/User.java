864b3de58fa08d70999fb5759368a0df1a020d79
kmozdzen <kmozdzen2@Onet.pl>
2023-10-04 18:27:14 +0200

    user rest api

---
+++ backend/marathon/src/main/java/com/kmozdzen/marathon/entity/User.java
@@ -0,0 +1,24 @@
+package com.kmozdzen.marathon.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NonNull;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ backend/marathon/src/main/java/com/kmozdzen/marathon/respository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kmozdzen.marathon.respository;
+
+
+import com.kmozdzen.marathon.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
